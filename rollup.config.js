36debd898e2364948655f8b28f70ebfa3359b62f
Vsevolod Kulagin <k00lagin@gmail.com>
2020-05-05 16:42:15 +0500

    Отключил импорт livereload  за ненадобностью

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
+// import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 
 const production = !process.env.ROLLUP_WATCH;
